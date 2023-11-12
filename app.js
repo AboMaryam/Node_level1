eb52972f63316242c249a4775683d950ea18a2fd
unknown <gafaralzake@gmail.com>
2023-11-12 13:42:49 +0200

    level2

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+const mongoose = require('mongoose');
 app.get('/', (req, res) => {
   res.sendFile('./public/views/home.html', {root:__dirname})
 })
 
-app.listen(port, () => {
+
+
+mongoose
+.connect('mongodb+srv://altyar:lM5iJR9s0MZ4ol2e@cluster0.fwua2ar.mongodb.net/all-data?retryWrites=true&w=majority')
+.then(()=>{
+  app.listen(port, () => {
   console.log(`http://localhost:${port}/`)
 })
+})
+.catch((err)=>{console.log(err)});
